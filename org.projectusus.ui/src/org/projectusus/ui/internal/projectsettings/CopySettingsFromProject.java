6fece410185c4cb4d791cfe90ebbc8db33ca4463
Stefan Schürle <s.w.schuerle@gmail.com>
2009-10-13 21:24:29 +0000

    new copy settings from project command

---
+++ org.projectusus.ui/src/org/projectusus/ui/internal/projectsettings/CopySettingsFromProject.java
@@ -0,0 +1,13 @@
+package org.projectusus.ui.internal.projectsettings;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CopySettingsFromProject extends AbstractHandler {
+
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+        return null;
+    }
+
+}
