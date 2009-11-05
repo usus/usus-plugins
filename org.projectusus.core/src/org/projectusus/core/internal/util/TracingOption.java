f6989ccb8e4f2921260f4116936fdf95125d992b
Leif Frenzel <himself@leiffrenzel.de>
2009-11-05 19:50:40 +0000

    added trace for SQI computations

---
+++ org.projectusus.core/src/org/projectusus/core/internal/util/TracingOption.java
@@ -0,0 +1,41 @@
+// Copyright (c) 2009 by the projectusus.org contributors
+// This software is released under the terms and conditions
+// of the Eclipse Public License (EPL) 1.0.
+// See http://www.eclipse.org/legal/epl-v10.html for details.
+package org.projectusus.core.internal.util;
+
+import static org.eclipse.core.runtime.Platform.getDebugOption;
+
+import org.projectusus.core.internal.UsusCorePlugin;
+
+public enum TracingOption {
+
+    SQI( "[SQI]" ); //$NON-NLS-1$
+
+    private final String tracePrefix;
+    private final boolean tracing;
+
+    private TracingOption( String tracePrefix ) {
+        this.tracePrefix = tracePrefix;
+        this.tracing = isTracing();
+    }
+
+    public void trace( String content ) {
+        if( tracing ) {
+            System.out.println( tracePrefix + " " + content ); //$NON-NLS-1$
+        }
+    }
+
+    public void trace( Throwable thr ) {
+        if( tracing ) {
+            trace( thr.getMessage() );
+            thr.printStackTrace();
+        }
+    }
+
+    private boolean isTracing() {
+        String option = UsusCorePlugin.getPluginId() + "/" + toString(); //$NON-NLS-1$
+        String value = getDebugOption( option );
+        return value != null && value.equalsIgnoreCase( "true" ); //$NON-NLS-1$
+    }
+}
