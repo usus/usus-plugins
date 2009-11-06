9bb33aeb3598b1fde0c8c43f75a01e1830381b41
Leif Frenzel <himself@leiffrenzel.de>
2009-11-06 13:07:30 +0000

    minor cleanup

--- org.projectusus.core/src/org/projectusus/core/internal/proportions/sqi/jdtdriver/FileDriver.java
+++ org.projectusus.core/src/org/projectusus/core/internal/proportions/sqi/jdtdriver/FileDriver.java
@@ -7,15 +7,14 @@ package org.projectusus.core.internal.proportions.sqi.jdtdriver;
 import static java.util.Arrays.asList;
 import static org.eclipse.jdt.core.JavaCore.createCompilationUnitFrom;
 import static org.eclipse.jdt.core.dom.ASTParser.newParser;
-import static org.projectusus.core.internal.util.TracingOption.SQI;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 class FileDriver {
 
@@ -29,7 +28,7 @@ class FileDriver {
         ICompilationUnit compilationUnit = createCompilationUnitFrom( file );
         CompilationUnit parse = parse( compilationUnit );
 
-        for( ASTVisitor visitor : asList( new MethodVisitor(), new ClassVisitor(), new ML(), new CC() ) ) {
+        for( ASTVisitor visitor : asList( new ClassVisitor(), new ML(), new CC() ) ) {
             parse.accept( visitor );
         }
     }
@@ -39,15 +38,6 @@ class FileDriver {
         parser.setKind( ASTParser.K_COMPILATION_UNIT );
         parser.setSource( unit );
         parser.setResolveBindings( true );
-        return (CompilationUnit)parser.createAST( null ); // parse
-    }
-
-    // TODO presumably implement an AST visitor per Isis check
-    class MethodVisitor extends ASTVisitor {
-        @Override
-        public boolean visit( MethodDeclaration node ) {
-            SQI.trace( node.getName().toString() );
-            return super.visit( node );
-        }
+        return (CompilationUnit)parser.createAST( new NullProgressMonitor() );
     }
 }
