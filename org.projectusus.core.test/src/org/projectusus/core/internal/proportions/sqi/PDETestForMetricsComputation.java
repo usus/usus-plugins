c45201b7207c5bda24a91b5c54f0d9569fb4598a
Leif Frenzel <himself@leiffrenzel.de>
2009-11-08 10:38:53 +0000

    basic PDE test setup for JDT-based metrics computations

---
+++ org.projectusus.core.test/src/org/projectusus/core/internal/proportions/sqi/PDETestForMetricsComputation.java
@@ -0,0 +1,54 @@
+// Copyright (c) 2009 by the projectusus.org contributors
+// This software is released under the terms and conditions
+// of the Eclipse Public License (EPL) 1.0.
+// See http://www.eclipse.org/legal/epl-v10.html for details.
+package org.projectusus.core.internal.proportions.sqi;
+
+import static org.junit.Assert.*;
+import static org.projectusus.core.internal.proportions.sqi.IsisMetrics.ML;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.Platform;
+import org.junit.Test;
+import org.projectusus.core.internal.PDETestUsingWSProject;
+import org.projectusus.core.internal.proportions.sqi.jdtdriver.FileDriver;
+
+public class PDETestForMetricsComputation extends PDETestUsingWSProject{
+
+    @Test
+    public void simpleCase() throws Exception {
+        IFile file = createWSFile( "A.java", loadContent("A.java") );
+        new FileDriver( file ).compute();
+        ProjectResults results = WorkspaceResults.getInstance().getProjectResults( file.getProject() );
+        assertEquals( 1, results.getViolationCount( ML ) );
+        assertEquals( 2, results.getViolationBasis( ML ) );
+    }
+    
+    private String loadContent(String fileName) throws Exception {
+        URL entry = loadEntry( fileName );
+        return readPreservingBinaryIdentity( entry.openStream());
+    }
+
+    private URL loadEntry( String fileName ) {
+        return Platform.getBundle( "org.projectusus.core.test" ).getEntry( "resources/" + fileName );
+    }
+    
+    private String readPreservingBinaryIdentity( InputStream is ) throws IOException {
+        StringBuffer result = new StringBuffer();
+        Reader reader = new InputStreamReader( is );
+        char[] buf = new char[512];
+        int charsRead = 0;
+        do {
+            result.append( String.valueOf( buf, 0, charsRead ) );
+            charsRead = reader.read( buf );
+        } while( charsRead > -1 );
+        return result.toString();
+    }
+
+}
