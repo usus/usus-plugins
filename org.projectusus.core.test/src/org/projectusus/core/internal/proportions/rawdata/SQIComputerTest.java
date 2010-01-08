c492e14583d8a7a60cac37e15df58525bb009f1a
Stefan Schürle <s.w.schuerle@gmail.com>
2010-01-08 09:46:27 +0000

    assertthat is not available in older junit versions

--- org.projectusus.core.test/src/org/projectusus/core/internal/proportions/rawdata/SQIComputerTest.java
+++ org.projectusus.core.test/src/org/projectusus/core/internal/proportions/rawdata/SQIComputerTest.java
@@ -4,8 +4,7 @@
 // See http://www.eclipse.org/legal/epl-v10.html for details.
 package org.projectusus.core.internal.proportions.rawdata;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.projectusus.core.internal.proportions.rawdata.CodeProportionKind.ML;
 
 import org.junit.Test;
@@ -15,12 +14,12 @@ public class SQIComputerTest {
     @Test
     public void zeroBasisYieldsZero() {
         SQIComputer computer = new SQIComputer( 0, 42, ML );
-        assertThat( computer.compute(), is( 0.0 ) );
+        assertEquals(0.0, computer.compute());
     }
     
     @Test
     public void noViolationsYields100Percent() {
         SQIComputer computer = new SQIComputer( 10, 0, ML );
-        assertThat( computer.compute(), is( 100.0 ) );
+        assertEquals( 100.0, computer.compute());
     }
 }
