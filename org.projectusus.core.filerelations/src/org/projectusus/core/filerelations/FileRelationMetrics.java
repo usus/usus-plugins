ca50ddbf9036f4e4492a27c5ac9032adb3a30642
Nicole Rauch <nicole.m@gmx.de>
2010-02-17 09:13:03 +0000

    Schnittstellenklasse FileRelationMetrics angefangen.

---
+++ org.projectusus.core.filerelations/src/org/projectusus/core/filerelations/FileRelationMetrics.java
@@ -0,0 +1,15 @@
+package org.projectusus.core.filerelations;
+
+public class FileRelationMetrics {
+
+	private FileRelations relations;
+	
+	public FileRelationMetrics(FileRelations relations){
+		this.relations = relations;
+	}
+	
+//	public int getRelativeACD(int totalNumberOfClasses){
+//		new ACDCalculator(relations);
+//	}
+	
+}
