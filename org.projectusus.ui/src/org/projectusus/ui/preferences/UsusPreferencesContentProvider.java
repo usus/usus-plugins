ec5f87a2b04e41cef5c3d746bf39932c66649810
Andreas Leidig <derleider@web.de>
2010-07-18 16:35:53 +0000

    committing in parts, cuz svn sucks

---
+++ org.projectusus.ui/src/org/projectusus/ui/preferences/UsusPreferencesContentProvider.java
@@ -0,0 +1,24 @@
+package org.projectusus.ui.preferences;
+
+import java.util.Set;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.projectusus.core.statistics.CockpitExtensionPref;
+
+public class UsusPreferencesContentProvider implements IStructuredContentProvider {
+
+    public void dispose() {
+        // not needed
+    }
+
+    public void inputChanged( Viewer viewer, Object oldInput, Object newInput ) {
+        // not needed
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public Object[] getElements( Object inputElement ) {
+        return ((Set<CockpitExtensionPref>)inputElement).toArray();
+    }
+
+}
