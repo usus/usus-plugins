fd9edf11cfd50b891f8a26da8c0ce14338be87ae
Andreas Leidig <derleider@web.de>
2010-08-02 18:26:03 +0000

    Sets work much better with cool equals implementations...

--- org.projectusus.core/src/org/projectusus/core/basis/PackageHotspot.java
+++ org.projectusus.core/src/org/projectusus/core/basis/PackageHotspot.java
@@ -1,5 +1,7 @@
 package org.projectusus.core.basis;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.projectusus.core.filerelations.model.Packagename;
 
 public class PackageHotspot implements Hotspot {
@@ -26,6 +28,20 @@ public class PackageHotspot implements Hotspot {
 
     public int compareTo( Hotspot other ) {
         return getName().compareTo( other.getName() );
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+        if( !(obj instanceof PackageHotspot) ) {
+            return false;
+        }
+        PackageHotspot other = (PackageHotspot)obj;
+        return new EqualsBuilder().append( getName(), other.getName() ).append( getPath(), other.getPath() ).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append( getName() ).append( getPath() ).toHashCode();
     }
 
 }
