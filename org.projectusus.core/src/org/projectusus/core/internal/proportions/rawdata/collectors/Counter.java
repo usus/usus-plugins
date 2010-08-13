c65fd50b2f375d3106847b417a57a1a567689466
Andreas Leidig <derleider@web.de>
2010-08-13 09:35:06 +0000

    garbage collect

--- org.projectusus.core/src/org/projectusus/core/internal/proportions/rawdata/collectors/Counter.java
+++ org.projectusus.core/src/org/projectusus/core/internal/proportions/rawdata/collectors/Counter.java
@@ -4,43 +4,25 @@ import java.util.Stack;
 
 public class Counter {
     private Stack<Integer> counts = new Stack<Integer>();
-    private String currentThread = "";
-    {
-        currentThread = getCurrentThreadName();
-    }
 
     public void startNewCount() {
         startNewCount( 0 );
     }
 
     public void startNewCount( int startValue ) {
-        checkCurrentThread();
         counts.push( new Integer( startValue ) );
     }
 
     public int getAndClearCount() {
-        checkCurrentThread();
         if( counts.size() == 0 ) {
-            System.out.println( "empty stack" );
             return 0;
         }
-        Integer value = counts.pop();
-        return value.intValue();
+        return counts.pop().intValue();
     }
 
     public void increaseLastCountBy( int amount ) {
         int newValue = amount + getAndClearCount();
         startNewCount( newValue );
-    }
-
-    private void checkCurrentThread() {
-        if( !currentThread.equals( getCurrentThreadName() ) ) {
-            System.out.println( "Threadname geändert" );
-        }
-    }
-
-    private String getCurrentThreadName() {
-        return Thread.currentThread().toString();
     }
 
 }
