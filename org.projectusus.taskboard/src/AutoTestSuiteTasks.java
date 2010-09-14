82affccebc9841c9cb6ddccdca07acd7ec4093ad
Marc Philipp <mail@marcphilipp.de>
2010-09-14 14:50:39 +0000

    autotestsuite: Added task board

---
+++ org.projectusus.taskboard/src/AutoTestSuiteTasks.java
@@ -0,0 +1,31 @@
+public class AutoTestSuiteTasks {
+	
+	void NamenDerExtendedSuiteUeberdenken() {
+		// TODO "All Tests Suite"?
+	}
+	
+	void CompatibilityProblem() {
+		// TODO Sollte zumindest mit 3.5 laufen
+	}
+	
+	void PaketAufteilen() {
+		// TODO Logik von UI trennen
+	}
+	
+	// Nach dem Release
+	
+	void EclEmma() {
+		// TODO Separates Plugin, das analog zu EclEmma launch shortcuts, tab groups, commands, 
+		// etc. definiert
+	}
+	
+	void PDE() {
+		// TODO Separates Plugin zum Ausführen von PDE-Tests für mehrere Projekte, 
+		// das Abhängigkeit zu JUnit-PDE-Plugin (?) hat  
+	}
+	
+	void TestklassenFilterbarMachen() {
+		// TODO Testklassen filterbar machen, erste Iteration: per RegEx auf Klassennamen?
+	}
+
+}
