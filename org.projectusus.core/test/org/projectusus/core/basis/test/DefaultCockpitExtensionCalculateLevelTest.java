d500f1c6511290da3ba00966122dd7496a29906f
Nicole Rauch <nicole.m@gmx.de>
2010-09-14 20:14:59 +0000

    Test was red for me (results were not adapted to the percentage display)

--- org.projectusus.core/test/org/projectusus/core/basis/test/DefaultCockpitExtensionCalculateLevelTest.java
+++ org.projectusus.core/test/org/projectusus/core/basis/test/DefaultCockpitExtensionCalculateLevelTest.java
@@ -18,11 +18,11 @@ public class DefaultCockpitExtensionCalculateLevelTest {
     public void testComputeAverage() {
         assertEquals( 0, DefaultCockpitExtension.calculateAverage( 0, 0 ), DELTA );
         assertEquals( 0, DefaultCockpitExtension.calculateAverage( 0, 10 ), DELTA );
-        assertEquals( 1, DefaultCockpitExtension.calculateAverage( 10, 10 ), DELTA );
-        assertEquals( 0.1, DefaultCockpitExtension.calculateAverage( 10, 100 ), DELTA );
-        assertEquals( 0.9, DefaultCockpitExtension.calculateAverage( 9, 10 ), DELTA );
-        assertEquals( 10, DefaultCockpitExtension.calculateAverage( 1000, 100 ), DELTA );
+        assertEquals( 100.0, DefaultCockpitExtension.calculateAverage( 10, 10 ), DELTA );
+        assertEquals( 10.0, DefaultCockpitExtension.calculateAverage( 10, 100 ), DELTA );
+        assertEquals( 90.0, DefaultCockpitExtension.calculateAverage( 9, 10 ), DELTA );
+        assertEquals( 100.0, DefaultCockpitExtension.calculateAverage( 1000, 100 ), DELTA );
         assertEquals( 0, DefaultCockpitExtension.calculateAverage( 0, 100 ), DELTA );
-        assertEquals( .25, DefaultCockpitExtension.calculateAverage( 1, 4 ), DELTA );
+        assertEquals( 25.0, DefaultCockpitExtension.calculateAverage( 1, 4 ), DELTA );
     }
 }
