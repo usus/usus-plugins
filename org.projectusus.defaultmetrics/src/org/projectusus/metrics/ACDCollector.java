99bdcd7d6a116a787b3384d8911718ba44b0fe44
Nicole Rauch <nicole.m@gmx.de>
2012-11-01 16:03:49 +0100

    Corrected and improved ACD analysis.

--- org.projectusus.defaultmetrics/src/org/projectusus/metrics/ACDCollector.java
+++ org.projectusus.defaultmetrics/src/org/projectusus/metrics/ACDCollector.java
@@ -1,14 +1,11 @@
 package org.projectusus.metrics;
 
-import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.FieldAccess;
-import org.eclipse.jdt.core.dom.IBinding;
-import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.SimpleType;
@@ -21,10 +18,6 @@ import org.projectusus.core.metrics.MetricsCollector;
 public class ACDCollector extends MetricsCollector {
 
     private WrappedTypeBinding currentType;
-
-    public ACDCollector() {
-        super();
-    }
 
     @Override
     public boolean visit( TypeDeclaration node ) {
@@ -60,70 +53,42 @@ public class ACDCollector extends MetricsCollector {
 
     @Override
     public boolean visit( SimpleType node ) {
-        visitType( BoundTypeConverter.wrap( node ) );
-        return false;
+        return connectCurrentTypeAnd( BoundTypeConverter.wrap( node ) );
     }
 
     // expressions
 
     @Override
     public boolean visit( SimpleName node ) {
-        if( !node.isDeclaration() ) {
-            IBinding binding = node.resolveBinding();
-            if( binding instanceof IVariableBinding ) {
-                visitType( BoundTypeConverter.wrap( (IVariableBinding)binding ) );
-            }
+        if( node.isDeclaration() ) {
+            return false;
         }
-        return false;
+        return connectCurrentTypeAnd( BoundTypeConverter.wrap( node ) );
     }
-
-    // echt überflüssig, wie es scheint:
-    // @Override
-    // public boolean visit( QualifiedName node ) {
-    // Name qualifier = node.getQualifier();
-    // if( qualifier == null ) {
-    // return false;
-    // }
-    // if( qualifier.isSimpleName() ) {
-    // visitType( BoundType.of( (SimpleName)qualifier ) );
-    // } else if( qualifier.isQualifiedName() ) {
-    // visitType( BoundType.of( (QualifiedName)qualifier ) );
-    // }
-    // return false;
-    // }
 
     @Override
     public boolean visit( MethodInvocation node ) {
         Expression targetExpression = node.getExpression();
-        if( targetExpression != null ) {
-            visitExpression( targetExpression );
-        } else {
-            visitType( BoundTypeConverter.wrap( node ) );
+        if( targetExpression == null ) {
+            return connectCurrentTypeAnd( BoundTypeConverter.wrap( node ) );
         }
-        return false;
+        return true;
     }
 
     @Override
     public boolean visit( FieldAccess node ) {
         Expression targetExpression = node.getExpression();
-        if( targetExpression != null ) {
-            visitExpression( targetExpression );
-        } else {
-            visitType( BoundTypeConverter.wrap( node ) );
+        if( targetExpression == null ) {
+            return connectCurrentTypeAnd( BoundTypeConverter.wrap( node ) );
         }
-        return false;
+        return true;
     }
 
-    private void visitType( WrappedTypeBinding targetType ) {
+    private boolean connectCurrentTypeAnd( WrappedTypeBinding targetType ) {
         if( currentType != null && targetType != null && targetType.isValid() && !currentType.equals( targetType ) ) {
             getMetricsWriter().addClassReference( currentType, targetType );
         }
-    }
-
-    private void visitExpression( Expression targetExpression ) {
-        if( targetExpression.getNodeType() == ASTNode.SIMPLE_NAME ) {
-            visitType( BoundTypeConverter.wrap( (SimpleName)targetExpression ) );
-        }
+        return false;
     }
 
     private void setCurrentType( AbstractTypeDeclaration node ) {
