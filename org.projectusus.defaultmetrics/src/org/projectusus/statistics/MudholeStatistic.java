a07818f4c98fe79af948300fdc4bf072279cb9b6
Nicole Rauch <nicole.m@gmx.de>
2012-11-25 21:22:56 +0100

    First experimental version of a mudhole statistic.

--- org.projectusus.defaultmetrics/src/org/projectusus/statistics/MudholeStatistic.java
+++ org.projectusus.defaultmetrics/src/org/projectusus/statistics/MudholeStatistic.java
@@ -6,19 +6,32 @@ import org.projectusus.core.statistics.CockpitExtension;
 
 public class MudholeStatistic extends CockpitExtension {
 
-    private static int KG_LIMIT = 20;
-    private ACDStatistic acdStatistic = new ACDStatistic();
+    private static final int MUDHOLE_LIMIT = 49;
 
-    // private CyclomaticComplexityStatistic ccStatistic = new CyclomaticComplexityStatistic();
+    // class metrics
+    private ClassSizeStatistic classSize = new ClassSizeStatistic();
+
+    // method metrics
+    private MethodLengthStatistic methodLength = new MethodLengthStatistic();
+    private CyclomaticComplexityStatistic cc = new CyclomaticComplexityStatistic();
+
+    private int currentClassValue;
 
     public MudholeStatistic() {
-        super( codeProportionUnit_CLASS_label, KG_LIMIT );
+        super( codeProportionUnit_METHOD_label, MUDHOLE_LIMIT );
     }
 
     @Override
     public void inspectClass( SourceCodeLocation location, MetricsResults results ) {
-        addResult( location, results.getIntValue( MetricsResults.METHODS ) );
-        acdStatistic.inspectClass( location, results );
+        int result = classSize.valueForClass( results );
+        currentClassValue = result >= ClassSizeStatistic.KG_LIMIT ? result : 0;
+    }
+
+    @Override
+    public void inspectMethod( SourceCodeLocation location, MetricsResults results ) {
+        int methodValue = methodLength.valueForMethod( results ) * cc.valueForMethod( results );
+        methodValue += currentClassValue;
+        addResult( location, methodValue );
     }
 
     @Override
