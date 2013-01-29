a58b9db9f8c292eb83919764f0675c9f2231a5d0
Nicole Rauch <nicole.m@gmx.de>
2013-01-29 20:24:13 +0100

    Streamlined the LinearCCStatisticTest in order to prepare for the next StatisticTests to come.

---
+++ org.projectusus.defaultmetrics/test/org/projectusus/statistics/test/StatisticTestBase.java
@@ -0,0 +1,19 @@
+package org.projectusus.statistics.test;
+
+import org.projectusus.core.basis.MetricsResults;
+import org.projectusus.core.basis.SourceCodeLocation;
+import org.projectusus.core.statistics.CockpitExtension;
+
+public class StatisticTestBase {
+
+    protected SourceCodeLocation location = new SourceCodeLocation( "location", 0, 0 );
+    protected MetricsResults results = new MetricsResults();
+    protected CockpitExtension statistic;
+    protected String metricsLabel;
+
+    protected void inspectMethodMetricsValue( int value ) {
+        results.put( metricsLabel, value );
+        statistic.inspectMethod( location, results );
+    }
+
+}
