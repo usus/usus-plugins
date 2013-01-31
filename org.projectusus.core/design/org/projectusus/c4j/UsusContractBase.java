1c99115198b2998bf68f1afcc84c95a946947271
Nicole Rauch <nicole.m@gmx.de>
2013-01-31 23:51:27 +0100

    Contracts should indeed fail! And we should write something to the console in case the writer was not setup.

--- org.projectusus.core/design/org/projectusus/c4j/UsusContractBase.java
+++ org.projectusus.core/design/org/projectusus/c4j/UsusContractBase.java
@@ -1,8 +1,6 @@
 package org.projectusus.c4j;
 
-
 import net.sourceforge.c4j.ContractBase;
-
 
 public class UsusContractBase<T> extends ContractBase<T> {
     public UsusContractBase( T target ) {
@@ -14,5 +12,6 @@ public class UsusContractBase<T> extends ContractBase<T> {
         String target = m_target.getClass().getName();
         String extendedMessage = "Contract: " + contract + ", Target: " + target + " - " + message;
         C4JFileWriter.assertStatic( condition, extendedMessage );
+        assert condition : extendedMessage;
     }
 }
