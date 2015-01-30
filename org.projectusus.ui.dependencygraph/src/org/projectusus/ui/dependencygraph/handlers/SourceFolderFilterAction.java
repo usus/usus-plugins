a4503a25f74555b6689938c97abc94b6a67a61ed
Marc Philipp <mail@marcphilipp.de>
2015-01-30 09:40:40 +0100

    Validation: At least one source folder needs to be selected

--- org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/handlers/SourceFolderFilterAction.java
+++ org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/handlers/SourceFolderFilterAction.java
@@ -1,24 +1,30 @@
 package org.projectusus.ui.dependencygraph.handlers;
 
 import static org.apache.commons.lang.StringUtils.join;
+import static org.eclipse.core.runtime.IStatus.ERROR;
+import static org.eclipse.core.runtime.IStatus.OK;
 import static org.eclipse.jdt.ui.ISharedImages.IMG_OBJS_PACKFRAG_ROOT;
-import static org.eclipse.jface.window.Window.OK;
 import static org.projectusus.ui.dependencygraph.DependencyGraphPlugin.plugin;
 
 import java.util.List;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.dialogs.ListSelectionDialog;
+import org.eclipse.ui.dialogs.CheckedTreeSelectionDialog;
+import org.eclipse.ui.dialogs.ISelectionStatusValidator;
 import org.eclipse.ui.dialogs.SelectionDialog;
 import org.projectusus.jfeet.selection.ElementsFrom;
+import org.projectusus.ui.dependencygraph.DependencyGraphPlugin;
 import org.projectusus.ui.dependencygraph.common.IRefreshable;
 import org.projectusus.ui.dependencygraph.filters.SourceFolderFilter;
 
@@ -63,11 +69,13 @@ public final class SourceFolderFilterAction extends Action {
     }
 
     private SelectionDialog createSelectionDialog( Shell shell ) {
-        ListSelectionDialog dialog = new ListSelectionDialog( shell, filter.getAllSourceFolders(), ArrayContentProvider.getInstance(), new SourceFolderLabelProvider(),
-                "Please select visible source folders:" );
+        CheckedTreeSelectionDialog dialog = new CheckedTreeSelectionDialog( shell, new SourceFolderLabelProvider(), new SourceFolderContentProvider() );
         dialog.setTitle( "Visible source folders" );
-        dialog.setInitialElementSelections( filter.getVisibleSourceFolders() );
+        dialog.setMessage( "Please select visible source folders:" );
         dialog.setHelpAvailable( false );
+        dialog.setInput( filter.getAllSourceFolders() );
+        dialog.setInitialElementSelections( filter.getVisibleSourceFolders() );
+        dialog.setValidator( new AtLeastOneSourceFolderValidator() );
         return dialog;
     }
 
@@ -84,10 +92,39 @@ public final class SourceFolderFilterAction extends Action {
         }
     }
 
-    private final class SourceFolderLabelProvider extends LabelProvider {
+    private final static class AtLeastOneSourceFolderValidator implements ISelectionStatusValidator {
+        public IStatus validate( Object[] selection ) {
+            if( selection.length > 0 ) {
+                return status( OK, "" );
+            }
+            return status( ERROR, "Please select at least one source folder." );
+        }
+
+        private IStatus status( int status, String message ) {
+            return new Status( status, DependencyGraphPlugin.PLUGIN_ID, message );
+        }
+    }
+
+    private final static class SourceFolderLabelProvider extends LabelProvider {
         @Override
         public Image getImage( Object element ) {
             return JavaUI.getSharedImages().getImage( IMG_OBJS_PACKFRAG_ROOT );
         }
     }
+
+    private final static class SourceFolderContentProvider extends ArrayContentProvider implements ITreeContentProvider {
+
+        public Object[] getChildren( Object parentElement ) {
+            return null;
+        }
+
+        public Object getParent( Object element ) {
+            return null;
+        }
+
+        public boolean hasChildren( Object element ) {
+            return false;
+        }
+    }
+
 }
