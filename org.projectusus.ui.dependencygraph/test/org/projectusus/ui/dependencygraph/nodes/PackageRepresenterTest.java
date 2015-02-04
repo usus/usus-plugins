usus/usus-plugins

commit 784d3ba45872e0320e2d4f1565709f9e30fe2714

show refactoring menu on graph nodes
The JDT refactoring menu is added to the graph node context menu, and
the graph or package nodes are adapted to Java elements to actually
enable the refactoring actions.

There is a small, but acceptable issue: The refactoring menu shows its
normal keyboard shortcuts, which don't work in the graph view.

org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/nodes/ClassRepresenter.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.ui.ISharedImages;
 import org.projectusus.core.filerelations.model.ClassDescriptor;
 import org.projectusus.core.filerelations.model.Packagename;
@@ -17,7 +19,7 @@ import ch.akuhn.foreach.ForEach;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 
-public class ClassRepresenter implements GraphNode {
+public class ClassRepresenter implements GraphNode, IAdaptable {
 
     private final ClassDescriptor clazz;
 
@@ -171,5 +173,12 @@ public class ClassRepresenter implements GraphNode {
 
     public boolean represents( IJavaElement javaElement ) {
         return javaElement.getResource().equals( getFile() );
+    }
+
+    public Object getAdapter( @SuppressWarnings( "rawtypes" ) Class adapter ) {
+        if( adapter.equals( IJavaElement.class ) ) {
+            return JavaCore.create( getFile() );
+        }
+        return null;
     }
 }

org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/nodes/PackageRepresenter.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.ui.ISharedImages;
 import org.eclipse.jdt.ui.JavaElementLabels;
@@ -14,7 +15,7 @@ import org.projectusus.core.filerelations.model.Packagename;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 
-public class PackageRepresenter implements GraphNode {
+public class PackageRepresenter implements GraphNode, IAdaptable {
 
     private final Packagename packagename;
     private static PackageRelations relations;
@@ -117,6 +118,13 @@ public class PackageRepresenter implements GraphNode {
     public boolean represents( IJavaElement javaElement ) {
         IJavaElement pkg = javaElement.getAncestor( IJavaElement.PACKAGE_FRAGMENT );
         return pkg != null && getRelatedPackage().getJavaElement().getElementName().equals( pkg.getElementName() );
+    }
+
+    public Object getAdapter( @SuppressWarnings( "rawtypes" ) Class adapter ) {
+        if( adapter.equals( IJavaElement.class ) ) {
+            return getPackagename().getJavaElement();
+        }
+        return null;
     }
 
 }

org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/common/DependencyGraphView.java
@@ -7,7 +7,12 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.ui.IContextMenuConstants;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jdt.ui.actions.ConvertingSelectionProvider;
+import org.eclipse.jdt.ui.actions.RefactorActionGroup;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
@@ -46,7 +51,7 @@ import org.projectusus.ui.dependencygraph.filters.PackagenameNodeFilter;
 import org.projectusus.ui.dependencygraph.handlers.ChangeZoom;
 import org.projectusus.ui.dependencygraph.nodes.GraphNode;
 
-public abstract class DependencyGraphView extends ViewPart implements IRestrictNodesFilterProvider, IShowInTarget, IZoomableWorkbenchPart {
+public abstract class DependencyGraphView extends ViewPart implements IRestrictNodesFilterProvider, IShowInTarget, IZoomableWorkbenchPart, IMenuListener {
 
     private final DependencyGraphModel model;
     private DependencyGraphViewer graphViewer;
@@ -57,6 +62,7 @@ public abstract class DependencyGraphView extends ViewPart implements IRestrictN
     private final HideNodesFilter hideNodesFilter = new HideNodesFilter(); // the nodes the user manually X-ed out
     private boolean restricting = false;
     private final IPartListener2 selectionSynchronizationListener = new SelectionSynchronizationListener( this );
+    private RefactorActionGroup refactorAction;
 
     public DependencyGraphView( DependencyGraphModel model ) {
         super();
@@ -77,6 +83,7 @@ public abstract class DependencyGraphView extends ViewPart implements IRestrictN
         refresh();
         extendSelectionBehavior();
         registerContextMenu( site );
+        refactorAction = new RefactorActionGroup( getSite(), new ConvertingSelectionProvider( site.getSelectionProvider() ) );
     }
 
     private void registerContextMenu( IViewSite site ) {
@@ -84,6 +91,13 @@ public abstract class DependencyGraphView extends ViewPart implements IRestrictN
         menuManager.add( new Separator( "nodeActions" ) );
         graphViewer.getGraphControl().setMenu( menuManager.createContextMenu( graphViewer.getControl() ) );
         site.registerContextMenu( menuManager, graphViewer );
+
+        createRefactoringMenu( menuManager );
+    }
+
+    private void createRefactoringMenu( MenuManager menuManager ) {
+        menuManager.add( new Separator( IContextMenuConstants.GROUP_REORGANIZE ) );
+        menuManager.addMenuListener( this );
     }
 
     private static Composite createComposite( Composite parent ) {
@@ -101,6 +115,12 @@ public abstract class DependencyGraphView extends ViewPart implements IRestrictN
         separator.setVisible( false );
         toolBar.add( separator );
         toolBar.add( new ChangeZoom( this ) );
+    }
+
+    public void menuAboutToShow( IMenuManager manager ) {
+        if( manager.find( RefactorActionGroup.MENU_ID ) == null ) {
+            refactorAction.fillContextMenu( manager );
+        }
     }
 
     public AbstractZoomableViewer getZoomableViewer() {
@@ -228,6 +248,9 @@ public abstract class DependencyGraphView extends ViewPart implements IRestrictN
         UsusModelProvider.ususModel().removeUsusModelListener( listener );
         graphViewer.getGraphControl().removeSelectionListener( selectionListener );
         registerEditorSynchronizationListener( false );
+        if( refactorAction != null ) {
+            refactorAction.dispose();
+        }
         super.dispose();
     }
 

commit 37a2b6035496692c486ed14d2c22e5902c19b826

Fix select all classes in same package action
This regression was introduced by 784d3ba. Both ClassRepresenter and
PackageRepresenter did not follow the recommendation in PlatformObject,
namely to consult the Platform's adapter manager when the cannot build
an adapter on their own.

org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/nodes/ClassRepresenter.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.ui.ISharedImages;
@@ -19,7 +19,7 @@ import ch.akuhn.foreach.ForEach;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 
-public class ClassRepresenter implements GraphNode, IAdaptable {
+public class ClassRepresenter extends PlatformObject implements GraphNode {
 
     private final ClassDescriptor clazz;
 
@@ -175,10 +175,11 @@ public class ClassRepresenter implements GraphNode, IAdaptable {
         return javaElement.getResource().equals( getFile() );
     }
 
+    @Override
     public Object getAdapter( @SuppressWarnings( "rawtypes" ) Class adapter ) {
         if( adapter.equals( IJavaElement.class ) ) {
             return JavaCore.create( getFile() );
         }
-        return null;
+        return super.getAdapter( adapter );
     }
 }

org.projectusus.ui.dependencygraph/src/org/projectusus/ui/dependencygraph/nodes/PackageRepresenter.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.ui.ISharedImages;
 import org.eclipse.jdt.ui.JavaElementLabels;
@@ -15,7 +15,7 @@ import org.projectusus.core.filerelations.model.Packagename;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 
-public class PackageRepresenter implements GraphNode, IAdaptable {
+public class PackageRepresenter extends PlatformObject implements GraphNode {
 
     private final Packagename packagename;
     private static PackageRelations relations;
@@ -120,11 +120,12 @@ public class PackageRepresenter implements GraphNode, IAdaptable {
         return pkg != null && getRelatedPackage().getJavaElement().getElementName().equals( pkg.getElementName() );
     }
 
+    @Override
     public Object getAdapter( @SuppressWarnings( "rawtypes" ) Class adapter ) {
         if( adapter.equals( IJavaElement.class ) ) {
             return getPackagename().getJavaElement();
         }
-        return null;
+        return super.getAdapter( adapter );
     }
 
 }

org.projectusus.ui.dependencygraph/test/org/projectusus/ui/dependencygraph/nodes/ClassRepresenterTest.java
@@ -1,6 +1,8 @@
-package org.projectusus.ui.dependencygraph.nodes.test;
+package org.projectusus.ui.dependencygraph.nodes;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -10,6 +12,7 @@ import java.util.Set;
 import org.junit.Test;
 import org.projectusus.core.filerelations.model.ClassDescriptor;
 import org.projectusus.ui.dependencygraph.nodes.ClassRepresenter;
+import org.projectusus.ui.dependencygraph.nodes.GraphNode;
 
 public class ClassRepresenterTest {
 
@@ -30,5 +33,12 @@ public class ClassRepresenterTest {
         children.remove( child2 );
         assertEquals( 1, clazz.getChildren().size() );
         assertEquals( 1, representer.getChildren().size() );
+    }
+
+    @Test
+    public void isAdaptableToGraphNode() {
+        ClassRepresenter representer = new ClassRepresenter( mock( ClassDescriptor.class ) );
+        Object adapter = representer.getAdapter( GraphNode.class );
+        assertThat( adapter, notNullValue() );
     }
 }

org.projectusus.ui.dependencygraph/test/org/projectusus/ui/dependencygraph/nodes/PackageRepresenterTest.java
@@ -1,0 +1,19 @@
+package org.projectusus.ui.dependencygraph.nodes;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.eclipse.jdt.core.IPackageFragment;
+import org.junit.Test;
+import org.projectusus.core.filerelations.model.Packagename;
+
+public class PackageRepresenterTest {
+
+    @Test
+    public void isAdaptableToGraphNode() {
+        PackageRepresenter representer = new PackageRepresenter( Packagename.of( "a", mock( IPackageFragment.class ) ) );
+        Object adapter = representer.getAdapter( GraphNode.class );
+        assertThat( adapter, notNullValue() );
+    }
+}